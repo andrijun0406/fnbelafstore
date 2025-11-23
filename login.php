0083587c060284e634e6ba2b2c20b63330badfb7
Andri Junardi Ahmad <Andri.Ahmad@dell.com>
2025-11-23 13:07:45 +0700

    update login page

--- login.php
+++ login.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login - F&amp;B ELAF Store</title>
+    <title>Login - F &amp; B ELAF Store</title>
 
     <!-- Bootstrap 5 CSS -->
     <link rel="stylesheet"
