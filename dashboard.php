31f4ab143a910f6ea9a0c0f23a8fc6708524c550
Andri Junardi Ahmad <Andri.Ahmad@dell.com>
2025-11-23 10:48:10 +0700

    perubahan dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['user_id'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
+    <title>Dashboard Utama</title>
     https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css
 </head>
 <body>
